7528e1e438028f48b15d30bdb2d59edaa3939477
JorgeCA91 <jo2409@hotmail.com>
2012-10-25 23:41:18 -0500

    Creando Proyecto CocinAPaty
    Subiendo el proyecto de CocinaPaty ( Java)

---
+++ CocinAPaty/src/cocinapaty/CocinAPaty.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cocinapaty;
+
+/**
+ *
+ * @author Jorge
+ */
+public class CocinAPaty {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
